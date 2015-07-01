48233830c8b296a87a3af07fdb833883f8124d96
Brexton Pham <brextonpham@stanford.edu>
2015-07-01 11:11:45 -0400

    Initial Commit

---
+++ CustomTransitions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTransitions
+//
+//  Created by Brexton Pham on 7/1/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTransitions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTransitions
+//
+//  Created by Brexton Pham on 7/1/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
